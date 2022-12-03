e45d6209b13da87c94b3f1ccdc9c0f246325d5f9
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-12-03 15:54:38 -0300

    Subindo as aulas 27 até 29

--- python-basico/aula_27-fatiamento_de_strings.py
+++ python-basico/aula_27-fatiamento_de_strings.py
@@ -3,8 +3,14 @@ Fatiamento ded strings
  012345678
  Olá mundo
 -987654321
-Fatiamento [i:f:p] [::]
+Fatiamento [i:f:p] [::] i-(início) f-(fim) p-(passo) - de quantos caracteres ele vai pular. 
 Obs.: a função len retorna a qauntidade de caracteres da strings
 """
 
 variavel = 'Olá mundo'
+print(variavel[4:])
+print(variavel[0:5])
+print(variavel[-8:-2])
+print(variavel[0:9:1])
+print(variavel[::-1])  # invertido
+print(len(variavel))
